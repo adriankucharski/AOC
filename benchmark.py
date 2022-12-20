98e1ff763bd9ec1d71ef75077feb9e521119f663
adriankucharski <adrian1998.25@live.com>
2022-12-20 12:50:28 +0100

    ADD BENCHMARK

---
+++ benchmark.py
@@ -0,0 +1,79 @@
+from typing import List, Tuple
+
+import numpy as np
+from tqdm import tqdm
+from tracker_seg import SegmentationTracker
+import cv2
+
+def load_labels(labels_path: str) -> List[Tuple[int, int, int, int]]:
+    with open(labels_path) as f:
+        labels = []
+        for line in f.readlines():
+            _x, _y, _w, _h  = [int(num) for num in line.split(' ')[1:5]]
+            labels.append((_y, _x, _w - _x, _h - _y))
+        return labels
+
+def IoU(box1, box2):
+    # Calculate the coordinates of the intersection box
+    x1 = max(box1[1], box2[1])
+    y1 = max(box1[0], box2[0])
+    x2 = min(box1[1] + box1[3], box2[1] + box2[3])
+    y2 = min(box1[0] + box1[2], box2[0] + box2[2])
+    
+    # Calculate the area of the intersection box
+    intersection = max(0, x2 - x1) * max(0, y2 - y1)
+    
+    # Calculate the area of the union
+    union = box1[2] * box1[3] + box2[2] * box2[3] - intersection
+    
+    # Calculate the IoU
+    iou = intersection / union
+    
+    return iou
+
+def video_benchmark(vpath: str, labels_path: str, tracker: SegmentationTracker, show = True, frames: Tuple[int, int] = None):
+    ious = []
+    video = cv2.VideoCapture(vpath)
+    labels = load_labels(labels_path)
+
+    fps = video.get(cv2.CAP_PROP_FPS)
+    if fps == 30.0:
+        labels = labels[::8]
+        
+    if frames is None:
+        frames = (1, len(labels))
+    else:
+        frames = (max(1, frames[0]), min(len(labels), frames[1]))
+
+    print(frames)
+
+    # Skip n frames
+    for _ in range((frames[0] - 1)):
+        video.read()
+
+    # Get first frame
+    pred_box = labels[frames[0] - 1]
+    _, prev_frame = video.read()
+    
+    for i in tqdm(range(*frames)):
+        ret, next_frame = video.read()
+        if ret == False:
+            break
+        pred_box = tracker.get_next_box(pred_box, prev_frame, next_frame)
+        prev_frame = next_frame
+
+        iou = IoU(pred_box, labels[i])
+        ious.append(iou)
+
+        if show: 
+            (y,x,w,h) = pred_box
+            img = cv2.rectangle(np.copy(next_frame), (x, y), (x + w, y + h), (0, 255, 0), 2)
+            (y,x,w,h) = labels[i]
+            img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 255), 2)
+            cv2.imshow("img2", img)
+        if cv2.waitKey(25) & 0xFF == ord("q"):
+            cv2.destroyAllWindows()
+            break
+            
+    cv2.destroyAllWindows()
+    return np.mean(np.asarray(ious) > 0.5), np.mean(np.asarray(ious) > 0.25), np.mean(ious)
---
+++ tracker_seg.py
@@ -0,0 +1,156 @@
+import math
+from matplotlib import pyplot as plt
+import numpy as np
+from skimage import metrics, filters, morphology
+import cv2
+from sklearn.metrics import jaccard_score
+from typing import Any, Tuple, Deque
+import pytictoc
+from sklearn import tree
+from functools import partial
+from skimage import data, segmentation, feature, future
+from sklearn.ensemble import (
+    RandomForestClassifier,
+    VotingClassifier,
+    GradientBoostingClassifier,
+    AdaBoostClassifier
+)
+from collections import deque
+from skimage import exposure
+from sklearn.svm import SVC, SVR
+from sklearn.gaussian_process import GaussianProcessClassifier
+
+class SegmentationTracker:
+    def __init__(
+        self,
+        fit_per_call=2,
+        n_estimators=25,
+        n_jobs=12,
+        max_depth=5,
+        max_samples=0.1,
+        sigma_min=0.5,
+        sigma_max=15,
+        intensity=True,
+        edges=True,
+        texture=True,
+        preprocess_gamma=0.5,
+        dataset_size = 5,
+        margin = 30,
+        as_circle_mask=False
+    ) -> None:
+        self.clf = RandomForestClassifier(
+            n_estimators=n_estimators,
+            n_jobs=n_jobs,
+            max_depth=max_depth,
+            max_samples=max_samples,
+            criterion="entropy",
+            class_weight="balanced_subsample",
+        )
+        self.features_func = partial(
+            feature.multiscale_basic_features,
+            intensity=intensity,
+            edges=edges,
+            texture=texture,
+            sigma_min=sigma_min,
+            sigma_max=sigma_max,
+            channel_axis=-1,
+        )
+        self.trainded = self.clf
+        self.gamma = preprocess_gamma
+        self.fit_counter = 0
+        self.fit_per_call = fit_per_call
+        self.margin = margin
+        self.as_circle_mask = as_circle_mask
+        self.training_data: Deque[Tuple[np.ndarray, np.ndarray]] = deque(maxlen=dataset_size)
+
+    def __fit(self):
+        if self.fit_counter % self.fit_per_call == 0:
+            _x, _y = [], []
+            for (xi,yi) in self.training_data:
+              xi = exposure.adjust_gamma(xi, self.gamma)
+              xi = self.features_func(xi)
+              _x.append(xi), _y.append(yi)
+            self.trainded = future.fit_segmenter(np.asarray(_y), np.asarray(_x), self.clf)
+        self.fit_counter += 1
+
+    def __predict(self, X: np.ndarray) -> np.ndarray:
+        X = exposure.adjust_gamma(X, self.gamma)
+        features = self.features_func(X)
+        return future.predict_segmenter(features, self.clf)
+
+    def __append_data(self, X: np.ndarray, y: np.ndarray):
+        self.training_data.append((X, y))
+
+    def __preprocess(self, im: np.ndarray) -> np.ndarray:
+        im = im / 255.0
+        return im
+
+    def reset(self):
+        self.training_data.clear()
+        self.trainded = self.clf
+        self.fit_counter = 0
+
+    def get_next_box(self, selected_box, prev_frame, next_frame) -> tuple:
+        prev_frame, next_frame = self.__preprocess(prev_frame), self.__preprocess(next_frame)
+        height, width = prev_frame.shape[:2]
+        y, x, w, h = selected_box
+        
+        index = SegmentationTracker.to_slice(selected_box, height, width, self.margin)
+        mask = SegmentationTracker.to_mask(selected_box, height, width, self.margin, self.as_circle_mask)
+        
+        prev_element = np.copy(prev_frame[index])
+        next_element = np.copy(next_frame[index])
+
+        self.__append_data(prev_element, mask)
+        self.__fit()
+        self.next_element_mask = self.__predict(next_element)
+
+        dy, dx = SegmentationTracker.fit_window(self.next_element_mask, (h, w), stride=4)
+        x = min(max(0, x - self.margin) + dx, width - w - self.margin)
+        y = min(max(0, y - self.margin) + dy, height - h - self.margin)
+
+        selected_box = (y, x, w, h)
+
+        return selected_box
+
+    @staticmethod
+    def fit_window(seg_im: np.ndarray, window_size: tuple, stride: int = 3) -> tuple:
+        seg_im = seg_im == seg_im.max()
+        seg_im = morphology.erosion(seg_im)
+        
+        windows = np.lib.stride_tricks.sliding_window_view(
+            seg_im, window_size, axis=(0, 1), writeable=False
+        )[::stride, ::stride]
+
+        values = dict()
+        for y in range(windows.shape[0]):
+            for x in range(windows.shape[1]):
+                values[(y * stride, x * stride)] = np.sum(windows[y, x])
+
+        return max(values, key=values.get)
+
+    @staticmethod
+    def to_mask(
+        box: tuple, height: int, width: int, margin: int, as_circle=False
+    ) -> np.ndarray:
+        (y, x, w, h) = box
+        mx = min(x, width - w)
+        my = min(y, height - h)
+        arr = np.ones((height, width))
+
+        if as_circle:
+            arr = cv2.circle(arr, (mx + w // 2, my + h // 2), (w + h) // 4, 2, -1)
+        else:
+            arr[my : my + h, mx : mx + w] = 2
+
+        x = min(max(0, x - margin), width - w - margin)
+        y = min(max(0, y - margin), height - h - margin)
+        index = np.s_[y : y + h + margin * 2, x : x + w + margin * 2]
+        return arr[index]
+
+    @staticmethod
+    def to_slice(box: tuple, height: int, width: int, margin: int) -> np.s_:
+        (y, x, w, h) = box
+        x = min(max(0, x - margin), width - w - margin)
+        y = min(max(0, y - margin), height - h - margin)
+        return np.s_[y : y + h + margin * 2, x : x + w + margin * 2]
